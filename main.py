bc9d8bb4f185eb404d72536f67415116cca5962f
Abhijithvenu25 <abhijithvenu25@gmail.com>
2024-09-23 19:17:50 +0530

    slight changes

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-import uvicorn,requests
+import uvicorn,requests,httpx
 from fastapi import FastAPI,Request
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5501",
+    "http://localhost:5501",
+]
+
+# Add CORS middleware to the FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows these origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 @app.get('/')
 async def default():
@@ -21,4 +37,8 @@ async def sended_requests(request:Request):
     headers = data.get('headers',{})
     body = data.get('body',{})
     token = data.get('token')
-    return 0
+
+    async with httpx.AsyncClient() as client:
+        response = await client.request(method, url, headers=headers, json=body)
+    
+    return response.json()
